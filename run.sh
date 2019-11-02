7e3b018ce07aca1b50b535a703c7b35608fb7e51
Douglas Black <dougblackjr@gmail.com>
2019-11-01 20:58:05 -0400

    One commit

---
+++ run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+DOMAINLIST="$(pwd)/domains.list"
+MAIL="/usr/bin/mail"
+EMAILS="doug@primitivesocial.com"
+
+OLDLOG="$(pwd)/monitordns.OLD"
+NEWLOG="$(pwd)/monitordns.CURRENT"
+TEMPLOG="$(pwd)/monitordns.$$"
+
+echo 	> $OLDLOG
+mv $NEWLOG $OLDLOG
+
+while read line
+do
+
+echo "Checking $line:" | tee -a $NEWLOG
+
+dig $line A +short | tee -a $NEWLOG
+
+echo "" | tee -a $NEWLOG
+
+done < $DOMAINLIST
+
+echo "-------------------------------------------------------------"
+
+diff -y --suppress-common-lines $OLDLOG $NEWLOG > $TEMPLOG
+
+if [ -s $TEMPLOG ] ; then
+
+  for EMAIL in $EMAILS
+
+  do
+
+    $MAIL -s "DNS status update" $EMAIL < $TEMPLOG
+
+  done
+
+fi
+
+rm -f $TEMPLOG
